58577d28085d425bbc4716aaabab4f00f2736db1
hypax <github@hypax.wtf>
2025-06-02 20:12:49 +0200

    Uses dynamic redirect URI for OAuth

--- auth/discord.php
+++ auth/discord.php
@@ -6,7 +6,8 @@ use League\OAuth2\Client\Provider\GenericProvider;
 $provider = new GenericProvider([
     'clientId'                => '1365759297841004564',
     'clientSecret'            => 'KFQ_tB_Jq7m4Q5b_s0LeOJ1UTmTA5EF6',
-    'redirectUri'             => 'https://dev.intrarp.de/auth/callback',
+    'redirectUri' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') .
+        '://' . $_SERVER['HTTP_HOST'] . '/auth/callback',
     'urlAuthorize'            => 'https://discord.com/api/oauth2/authorize',
     'urlAccessToken'          => 'https://discord.com/api/oauth2/token',
     'urlResourceOwnerDetails' => 'https://discord.com/api/users/@me',
