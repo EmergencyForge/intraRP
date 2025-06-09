b67b0734af8ffcdb50ee79b7bbeea78ae56ad3d9
hypax <github@hypax.wtf>
2025-06-09 15:50:34 +0200

    Update callback.php

--- auth/callback.php
+++ auth/callback.php
@@ -15,7 +15,7 @@ $provider = new GenericProvider([
     'clientId'                => $_ENV['DISCORD_CLIENT_ID'],
     'clientSecret'            => $_ENV['DISCORD_CLIENT_SECRET'],
     'redirectUri' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') .
-        '://' . $_SERVER['HTTP_HOST'] . '/auth/callback',
+        '://' . $_SERVER['HTTP_HOST'] . '/auth/callback.php',
     'urlAuthorize'            => 'https://discord.com/api/oauth2/authorize',
     'urlAccessToken'          => 'https://discord.com/api/oauth2/token',
     'urlResourceOwnerDetails' => 'https://discord.com/api/users/@me',
