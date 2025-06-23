444d211279c0e926df28d46a8596830893add99a
hypax <github@hypax.wtf>
2025-06-23 23:06:29 +0200

    Ensures config files are loaded on logout.

--- admin/logout.php
+++ admin/logout.php
@@ -2,4 +2,7 @@
 session_start();
 session_destroy();
 
+require __DIR__ . '/../assets/config/config.php';
+require __DIR__ . '/../assets/config/database.php';
+
 header('Location: ' . BASE_PATH . 'admin/login.php');
